d443002e048ad47d38017b46c8fe1fea8bd0bc2a
Zach Tellman <ztellman@gmail.com>
2013-10-29 16:19:11 -0700

    target collection-check 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-tuple "0.1.3"
+(defproject clj-tuple "0.1.4-SNAPSHOT"
   :description "Efficient small collections."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies []
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [criterium "0.4.1"]
-                                  [collection-check "0.1.0"]]}}
+                                  [collection-check "0.1.1-SNAPSHOT"]]}}
   :global-vars {*warn-on-reflection* true}
   :test-selectors {:benchmark :benchmark
                    :default (complement :benchmark)}
