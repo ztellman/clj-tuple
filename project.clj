5de6b5ef22ca05aeb2fbeb16f298d63856682635
Zach Tellman <ztellman@gmail.com>
2015-01-30 15:57:03 -0800

    this should really be at least a minor release, so tag 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-tuple "0.1.8"
+(defproject clj-tuple "0.2.0"
   :description "Efficient small collections."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
