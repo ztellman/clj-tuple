901df801da5f1968b593b6989af41ce026d25837
Zach Tellman <ztellman@gmail.com>
2015-01-30 15:48:47 -0800

    treat clojure as a :provided rather than :dev dependency

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies []
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                                  [criterium "0.4.1"]
-                                  [collection-check "0.1.5-SNAPSHOT"]]}}
+  :profiles {:dev {:dependencies [[criterium "0.4.1"]
+                                  [collection-check "0.1.5-SNAPSHOT"]]}
+             :provided {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
   :global-vars {*warn-on-reflection* true}
   :test-selectors {:benchmark :benchmark
                    :stress :stress
